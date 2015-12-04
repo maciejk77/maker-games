f6fdeb110e54d6f1bd7aec51c0de6b9c353dfa41
Maciej Kurek <maciejk77@gmail.com>
2015-12-04 14:09:38 +0000

    Minor refactoring

--- kata1.js
+++ kata1.js
@@ -1,7 +1,7 @@
 var newStr = "";
+str = str.split(""); 
 
 function splitter(str) {
-str = str.split(""); 
   for(var i = 0; i < str.length; i++) {
     if(i % 2 === 0) {
       newStr += str[i].toUpperCase();
@@ -11,8 +11,7 @@ str = str.split("");
   return newStr;  
 }
 
-function splitter2(str) {
-  str = str.split(""); 
+function splitter2(str) { 
   for(var i = 0; i < str.length; i++) {
     i % 2 === 0 ? newStr += str[i].toUpperCase() : newStr += str[i].toLowerCase(); 
   }
